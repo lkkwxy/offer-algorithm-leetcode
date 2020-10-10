b786b35dad0120ae00581b761c8a2c4a96fb30e0
lkkwxy <1403705809@qq.com>
2020-10-10 15:37:12 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ offer-algorithm-leetcode/offer-algorithm-leetcode/剑指 Offer 03. 数组中重复的数字.swift
@@ -0,0 +1,31 @@
+//
+//  剑指 Offer 03. 数组中重复的数字.swift
+//  offer-algorithm-leetcode
+//
+//  Created by 李坤 on 2020/10/10.
+//
+
+import Foundation
+/**
+ 找出数组中重复的数字。
+
+ 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+
+ 示例 1：
+
+ 输入：
+ [2, 3, 1, 0, 2, 5, 3]
+ 输出：2 或 3
+ */
+
+func findRepeatNumber(_ nums: [Int]) -> Int {
+    var set = Set<Int>()
+    for num in nums {
+        if set.contains(num) {
+            return num
+        }
+        set.insert(num)
+    }
+    return -1
+}
+
