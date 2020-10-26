e96a39df10427b03093201160fbd56e6d73011ae
likunkun <likunkun@czb365.com>
2020-10-26 23:12:19 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题

---
+++ offer-algorithm-leetcode/offer-algorithm-leetcode/剑指 Offer 10- II. 青蛙跳台阶问题.swift
@@ -0,0 +1,51 @@
+//
+//  剑指 Offer 10- II. 青蛙跳台阶问题.swift
+//  offer-algorithm-leetcode
+//
+//  Created by 李坤坤 on 2020/10/26.
+//
+
+import Foundation
+/**
+ 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ 
+ 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ 
+ 示例 1：
+ 
+ 输入：n = 2
+ 输出：2
+ 示例 2：
+ 
+ 输入：n = 7
+ 输出：21
+ 示例 3：
+ 
+ 输入：n = 0
+ 输出：1
+ 提示：
+ 
+ 0 <= n <= 100
+ 
+ 
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+func numWays(_ n: Int) -> Int {
+    if n <= 1 {
+        return 1
+    }
+    var result = 1
+    var perviousResult = 1
+    for _ in 2...n {
+        let tmp = result
+        result = result + perviousResult
+        perviousResult = tmp
+        if result > 1000000007 {
+            result = result % 1000000007
+        }
+    }
+    return result
+}
